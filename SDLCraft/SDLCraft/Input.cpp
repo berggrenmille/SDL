c01cd24654f301907cecd18b6fe19cce30cdaebf
iceklue <iceklue@gmail.com>
2017-11-08 19:17:15 +0100

    ye

--- SDLCraft/SDLCraft/Input.cpp
+++ SDLCraft/SDLCraft/Input.cpp
@@ -3,7 +3,16 @@
 
 namespace MolecularEngine
 {
-	bool Input::Test()
+	bool Input::exit = false;
+
+	bool Input::GetKeyDown(SDL_Keycode key)
+	{
+		if (key == SDLK_ESCAPE)
+			return true;
+		else
+			return false;
+	}
+	bool Input::GetKeyDown()
 	{
 		return false;
 	}
--- SDLCraft/SDLCraft/Input.h
+++ SDLCraft/SDLCraft/Input.h
@@ -1,9 +1,9 @@
-namespace MolecularEngine
-{
-	class Input;
-}
+#include "MolecularEngine.h"
 class MolecularEngine::Input
 {
 public:
-	static bool Test();
+	static bool GetKeyDown(SDL_Keycode key);
+	static bool GetKeyDown();
+private:
+	static bool exit;
 };
